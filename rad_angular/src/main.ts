edde9b4e544cf7eda8ffee950a86402bbc2aa0ca
ericalowanou-dot <ericalowanou@gmail.com>
2026-09-15 22:29:00 +0000

    Fix: retrait hydratation SSR incompatible avec zoneless

--- rad_angular/src/main.ts
+++ rad_angular/src/main.ts
@@ -2,11 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ],
 });
